f44962bdc0570d308d07cf3e1db28a7de8354ffe
francesco-scandale_amplifon <francesco.scandale@accenture.com>
2023-05-12 11:11:06 +0200

    test

--- script.ps1
+++ script.ps1
@@ -1,5 +1,33 @@
-set-psdebug -trace 1 # Used to show in the command line the executed commands
+set-psdebug -trace 0 # Used to show in the command line the executed commands
+#git config --global pager.branch false #used to disable paging in "git branch" 
+                                        #(paging could affect the behavior of the script)
 
-git add .
-git commit -m "first try"
-git push
+$currentBranch = git branch --show-current
+write-output "Current branch: $currentBranch"
+
+$allBranch = git branch -a
+write-output "List of branches: $allBranch"
+
+$flagBranchFound = 0
+
+while(!$flagBranchFound){
+    $chosenBranch = read-host "Choose a branch to write into"
+    write-output "Branch chosen: $chosenBranch"
+    
+    #for all available branches
+    foreach($branchI in $allBranch){
+        #assume branch is local, try to find it
+        if($branchI.equals("$chosenBranch") -or $branchI.equals("* $chosenBranch")){
+            $flagBranchFound = 1
+            write-output "valid branch: $branchI - flag: $flagBranchFound"
+        }
+    }
+
+    if(!$flagBranchFound) {write-output "Branch not found, please insert a valid branch"}
+}
+
+
+
+#git add .
+#git commit -m "first try"
+#git push
