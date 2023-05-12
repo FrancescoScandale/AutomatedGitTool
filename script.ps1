4140c0738a54b2d66499233a568906f8318b3879
francesco-scandale_amplifon <francesco.scandale@accenture.com>
2023-05-12 16:21:25 +0200

    test Silvio

--- script.ps1
+++ script.ps1
@@ -1,5 +1,6 @@
-set-psdebug -trace 1 # Used to show in the command line the executed commands
+set-psdebug -trace 1 #used to show in the command line the executed commands
 #git config --global pager.branch false #paging could affect the behavior of the script
+                                        #already set in my system
 
 #getting the remote repositories
 #config file uses global paths
@@ -18,24 +19,68 @@ $commitMessage = read-host "Insert the commit message"
 git add .
 git commit -m $commitMessage
 git push
-
-$currentBranch = git branch --show-current
-write-output "Current branch: $currentBranch"
+$modificationsBranch = git branch --show-current #retrieve current branch
+write-output "Current branch: $modificationsBranch"
 write-output ""
 write-output ""
 
+#merge into original branch
+write-output "MERGE INTO ORIGINAL BRANCH"
+$allBranch = git branch
+write-output "List of branches: $allBranch"
+
+$flagBranchFound = 0
+while(!$flagBranchFound){
+    $originalBranch = read-host "From which branch did you create this one?"
+    
+    #for all available branches
+    foreach($branchI in $allBranch){
+        if($branchI.equals("$originalBranch") -or $branchI.equals("* $originalBranch")){
+            $flagBranchFound = 1
+            write-output "Valid branch: $originalBranch - flag: $flagBranchFound"
+        }
+    }
+
+    if(!$flagBranchFound) {write-output "Branch not found, please insert a valid branch"}
+}
+
+git switch $originalBranch
+git fetch
+git pull
+git merge $modificationsBranch
+git push
+write-output ""
+write-output ""
+
+#write-output "MERGE INTO A TEMPORARY BRANCH FROM THE MAIN BRANCH"
+#$fromMain = read-host "What would you like to call the branch from main?"
+#git switch main
+#git fetch
+#git pull
+#git branch $fromMain
+#git switch $fromMain
+#git merge ###########
+#git push -u origin $fromMain
+
+
+#REMOTE REPOSITORIES
 write-output "MERGE REMOTE REPOSITORIES"
 for($i=0;$i -lt $remoteRepos.Length; $i++){
     set-location $remoteRepos[$i]
     get-location
     git fetch --all
-    git switch $currentBranch
+
+    git branch $modificationsBranch #create the temporary branch if not available
+    git switch $modificationsBranch
+    git fetch
+    git pull
     foreach($line in git remote){
         if($line -ne "origin"){
             $parentRepo = $line
         }
     }
-    git merge $parentRepo/$currentBranch --allow-unrelated-histories
+    git merge $parentRepo/$modificationsBranch --allow-unrelated-histories
+    git push
 
     write-output ""
     write-output ""
@@ -43,32 +88,12 @@ for($i=0;$i -lt $remoteRepos.Length; $i++){
 
 set-location ..\AutomatedGitTool
 
-#$allBranch = git branch -a
-#write-output "List of branches: $allBranch"
 
-#$flagBranchFound = 0
-
-#while(!$flagBranchFound){
-    #$chosenBranch = read-host "Choose a branch to write into"
-    #write-output "Branch chosen: $chosenBranch"
-    
-    ##for all available branches
-    #foreach($branchI in $allBranch){
-        ##assume branch is local, try to find it
-        #if($branchI.equals("$chosenBranch") -or $branchI.equals("* $chosenBranch")){
-            #$flagBranchFound = 1
-            #write-output "valid branch: $branchI - flag: $flagBranchFound"
-        #}
-    #}
-
-    #if(!$flagBranchFound) {write-output "Branch not found, please insert a valid branch"}
-#}
-
-
-
-
-##plan: i have changes here in the main repo on the branch X and have to merge them all
-##into the branch develop. Then need to merge this branch develop into the children repos,
-##in their own branch develop.
-##Assumption: at the moment the branch exists in the remote branches:
-    ##it does not have to be created by the script
+#chiedere prima di fare la modifica su ogni repo 
+#(potrebbe voler essere fatta solo su un country)
+    #chiedere su quale branch lo vuole portare 
+        #(fai un while, potrebbe dover essere portato su più branch)
+#quando chiedo se vuole allineare la realease di uno dei country, fai la merge
+    #direttamente dal develop appena allineato
+#controllo errori
+#creazione delle pull-request direttamente da riga di comando?
