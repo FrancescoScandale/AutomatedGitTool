371ac094ce98065a887e38bab98a2c1c66051d0b
francesco-scandale_amplifon <francesco.scandale@accenture.com>
2023-05-12 16:34:05 +0200

    test 5

--- script.ps1
+++ script.ps1
@@ -36,8 +36,7 @@ while(!$flagBranchFound){
     #for all available branches
     foreach($branchI in $allBranch){
         write-output "$branchI - $originalBranch"
-        #if($branchI.equals("$originalBranch") -or $branchI.equals("* $originalBranch")){
-        if(($originalBranch -contains $branchI)){
+        if($branchI.equals($originalBranch) -or $branchI.equals("* $originalBranch")){
             $flagBranchFound = 1
             write-output "Valid branch: $originalBranch - flag: $flagBranchFound"
         }
