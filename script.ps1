fd7547fc03252baf9a269cceaa07a551b1af87c3
francesco-scandale_amplifon <francesco.scandale@accenture.com>
2023-05-16 16:29:59 +0200

    fewer prints

--- script.ps1
+++ script.ps1
@@ -22,10 +22,10 @@ $modificationsBranch = git branch --show-current #retrieve current branch
 write-output "Current branch: $modificationsBranch"
 $commitMessage = read-host "Insert the commit message"
 write-output "Git does add, commit and push..."
-git push -u origin $modificationsBranch --porcelain
+git push -u origin $modificationsBranch --quiet
 git add .
-git commit -m $commitMessage --porcelain
-git push --porcelain
+git commit -m $commitMessage --quiet
+git push --quiet
 write-output "... done"
 write-output ""
 write-output ""
@@ -63,33 +63,32 @@ while($keepMerging.equals("y") -or $keepMerging.equals("Y")){
     git fetch --quiet
     git pull --quiet
 
-    $err = git merge $modificationsBranch
+    $err = git merge $modificationsBranch --quiet
     if(!($err.contains("fatal") -or $err.contains("failed")) -and !($originalBranch.contains("main"))){
-        git push --porcelain
+        git push --quiet
         
         if(!$originalBranch.contains("release")){
             #merge back into the original branch
             git switch $originalBranch
             git merge $modificationsBranch --quiet
-            git push --porcelain
+            git push --quiet
         }
 
         write-output "... done"
     } elseif($err.contains("fatal") -or $err.contains("failed")){
         write-output "...an error occurred, check the terminal"
     } else { #can't merge into main, just push the temporary branch
-        git push --porcelain
+        git push --quiet
 
         write-output "...to merge back into main, need to create a pull request from GitHub"
     }
-    write-output "... done"
+    write-output ""
 
     $keepMerging = read-host "Do you want to merge another branch? [y or Y if yes, any other if no]"    
-    write-output ""
-    write-output ""
 }
 
-
+write-output ""
+write-output ""
 
 #REMOTE REPOSITORIES
 write-output "MERGE REMOTE REPOSITORIES"
@@ -132,39 +131,39 @@ for($i=0;$i -lt $remoteRepos.Length; $i++){
                 #create the temporary branch and merge into it
                 git branch $modificationsBranch #(check: if the result of this command is not empty, another branch already has this name -> need to create it with another name)
                 git switch $modificationsBranch
-                git push -u origin $modificationsBranch --porcelain
+                git push -u origin $modificationsBranch --quiet
             }
             write-output "... done"
 
             write-output "Merging from origin, pushing, deleting temporary branch..."
             if($originalBranch.contains("release")){ #if in release, merge directly the local (aligned) develop
-                $err = git merge develop --allow-unrelated-histories
+                $err = git merge develop --allow-unrelated-histories --quiet
             } else {
                 foreach($line in git remote){
                     if($line -ne "origin"){
                         $parentRepo = $line
                     }
                 }
-                $err = git merge $parentRepo/$modificationsBranch --allow-unrelated-histories
+                $err = git merge $parentRepo/$modificationsBranch --allow-unrelated-histories --quiet
             }
             if(!($err.contains("fatal") -or $err.contains("failed")) -and !($originalBranch.contains("main"))){
-                git push --porcelain
+                git push --quiet
                 
                 if(!$originalBranch.contains("release")){
                     #merge back into the original branch
                     git switch $originalBranch
                     git merge $modificationsBranch --quiet
-                    git push --porcelain
+                    git push --quiet
 
                     git branch -D $modificationsBranch #force the delete
-                    git push origin -d $modificationsBranch --porcelain
+                    git push origin -d $modificationsBranch --quiet
                 }
 
                 write-output "... done"
             } elseif($err.contains("fatal") -or $err.contains("failed")){
                 write-output "...an error occurred, check the terminal"
             } else { #can't merge into main, just push the temporary branch
-                git push --porcelain
+                git push --quiet
 
                 write-output "...to merge back into main, need to create a pull request from GitHub"
             }
