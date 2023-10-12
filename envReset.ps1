aa47926a137259f7be5bdd18397094727b8a5883
FrancescoScandale <francesco.scandale00@gmail.com>
2023-10-12 14:27:56 +0200

    added environment reset script

---
+++ envReset.ps1
@@ -0,0 +1,28 @@
+git switch main
+git merge tempMain
+git push
+
+git branch -D tempMain
+git push origin -d tempMain
+
+git switch test
+
+
+Set-Location ..\AutomatedGitTool-1
+git fetch --all --prune --quiet
+git switch main
+git merge AutomatedGitTool/main --quiet
+git push --quiet
+
+git branch -D tempMain
+git push origin -d tempMain
+
+
+Set-Location ..\AutomatedGitTool-2
+git fetch --all --prune --quiet
+git switch main
+git merge AutomatedGitTool/main --quiet
+git push --quiet
+
+git branch -D tempMain
+git push origin -d tempMain
